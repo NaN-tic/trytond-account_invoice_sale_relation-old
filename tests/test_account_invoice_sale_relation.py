f321d39adfc9d199d335777b601d55f8e80c3ccc
jmartin <jmartin@zikzakmedia.com>
2014-11-07 13:40:55 +0100

    Cleanup tests

--- tests/test_account_invoice_sale_relation.py
+++ tests/test_account_invoice_sale_relation.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
 
 class AccountInvoiceSaleRelationTestCase(unittest.TestCase):
-    '''
-    Test Account Invoice Sale Relation module.
-    '''
+    'Test Account Invoice Sale Relation module'
 
     def setUp(self):
         module = 'account_invoice_sale_relation'
         trytond.tests.test_tryton.install_module(module)
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
