6d03a85cb7dd5f9139784d0235dbd1f9f0e90ca8
Raimon Esteve <raimon@nan-tic.com>
2017-01-12 21:53:26 +0100

    IMP register classes

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
+from . import invoice
+
 
 def register():
     Pool.register(
-        Invoice,
-        InvoiceLine,
+        invoice.Invoice,
+        invoice.InvoiceLine,
         module='account_invoice_sale_relation', type_='model')
